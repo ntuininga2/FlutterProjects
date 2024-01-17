3be354946cde9eb4dad07a5c1aca87634aff40fc
ntuininga2 <122303014+ntuininga2@users.noreply.github.com>
2024-01-17 09:21:46 -0700

    new project

--- testdrive/lib/main.dart
+++ testdrive/lib/main.dart
@@ -2,6 +2,10 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+
+import 'package:test_app/home/home.dart';
+
+
 void main() {
   runApp(MyApp());
 }
@@ -45,64 +49,64 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
 
-class _MyHomePageState extends State<MyHomePage> {
-  var selectedIndex = 0;
+// class _MyHomePageState extends State<MyHomePage> {
+//   var selectedIndex = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = GeneratorPage();
-        break;
-      case 1:
-        page = FavoritesPage();
-        break;
-      default:
-        throw UnimplementedError('no widget for $selectedIndex');
-    }
+//   @override
+//   Widget build(BuildContext context) {
+//     Widget page;
+//     switch (selectedIndex) {
+//       case 0:
+//         page = GeneratorPage();
+//         break;
+//       case 1:
+//         page = FavoritesPage();
+//         break;
+//       default:
+//         throw UnimplementedError('no widget for $selectedIndex');
+//     }
 
-    return Scaffold(
-      body: Row(
-        children: [
-          SafeArea(
-            child: NavigationRail(
-              extended: false,
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(Icons.home),
-                  label: Text('Home'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.favorite),
-                  label: Text('Favorites'),
-                ),
-              ],
-              selectedIndex: selectedIndex,
-              onDestinationSelected: (value) {
-                setState(() {
-                  selectedIndex = value;
-                });
-                //print('selected: $value');
-              },
-            ),
-          ),
-          Expanded(
-            child: Container(
-              color: Theme.of(context).colorScheme.primaryContainer,
-              child: page,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//     return Scaffold(
+//       body: Row(
+//         children: [
+//           SafeArea(
+//             child: NavigationRail(
+//               extended: false,
+//               destinations: [
+//                 NavigationRailDestination(
+//                   icon: Icon(Icons.home),
+//                   label: Text('Home'),
+//                 ),
+//                 NavigationRailDestination(
+//                   icon: Icon(Icons.favorite),
+//                   label: Text('Favorites'),
+//                 ),
+//               ],
+//               selectedIndex: selectedIndex,
+//               onDestinationSelected: (value) {
+//                 setState(() {
+//                   selectedIndex = value;
+//                 });
+//                 //print('selected: $value');
+//               },
+//             ),
+//           ),
+//           Expanded(
+//             child: Container(
+//               color: Theme.of(context).colorScheme.primaryContainer,
+//               child: page,
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 
 class FavoritesPage extends StatelessWidget {
   @override
---
+++ testingproject/android/app/src/main/kotlin/com/example/testingproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testingproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
